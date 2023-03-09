22c168cda771fe4cc323c2034b8d9d58b9bc751e
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-09 22:15:36 +0500

    hero end

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import './index.scss'
 import Header from '../../Layout/Header/Header'
+import Hero from '../../components/Hero/Hero'
 const index = () => {
   return (
     <>
-      <Header/>
+      <Header />
+      <Hero/>
     </>
   )
 }

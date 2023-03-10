b58b402b42de0bcf21a77e7948708de73ae712bb
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-10 22:21:19 +0500

    Express dostavka 24

---
+++ src/components/Express24/Express.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./express.scss";
+const Express = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Express;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,13 +4,15 @@ import Header from '../../Layout/Header/Header'
 import Hero from '../../components/Hero/Hero'
 import Menu from '../../components/Menu/Menu'
 import Cards from '../../components/Cards/Cards'
+import Express from '../../components/Express24/Express'
 const index = () => {
   return (
     <>
       <Header />
       <Hero />
       <Menu />
-      <Cards/>
+      <Cards />
+      <Express/>
     </>
   )
 }

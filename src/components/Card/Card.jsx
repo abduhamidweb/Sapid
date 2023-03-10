ae12a6efad19bdfe224bb91d272a436234464765
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-03-10 22:19:32 +0500

    cards end

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,19 +3,28 @@ import "./card.scss"
 const Card = () => {
     return (
         <>
-            <div className="card">
-                <div className="discount">-10%</div>
-                <div className="image-wrapper">
-                    <img src="https://via.placeholder.com/200x200" alt="product image" />
+            <div className="col-lg-3 col-md-6 co-sm-12 mx-auto my-3 px-4">
+                <div className="card">
+                    <div className="discount discount_top">-10%</div>
+                    <div className="image-wrapper">
+                        <img src="https://via.placeholder.com/200x200" alt="product image" />
+                    </div>
+                    <div className="header__body">
+                        <div className="title">Product title</div>
+                        <div className="description">Product description</div>
+                    </div>
+                    <div className="header__footer">
+                        <div className="price ">
+                            <h6>$50132 sum</h6>
+                            <span className="discount">$61320</span>
+                        </div>
+                        <div className="btn_wrpaper">
+                            <button className="button">Buy now</button>
+                        </div>
+                    </div>
                 </div>
-                <div className="title">Product title</div>
-                <div className="description">Product description</div>
-                <div className="price">
-                    $50
-                    <span className="discount">$60</span>
-                </div>
-                <a href="#" className="button">Buy now</a>
-            </div> 
+            </div>
+
         </>
     );
 };
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,9 +7,14 @@ const Cards = () => {
             <section className='cards-wrapper'>
                 <div className="container">
                     <div className="row my-5">
-                        <div className="col-lg-3 col-md-6 co-sm-12 mx-auto my-3 px-4">
-                            <Card />
-                        </div>
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
                     </div>
                 </div>
             </section>
